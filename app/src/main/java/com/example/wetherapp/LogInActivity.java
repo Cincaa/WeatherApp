7f7a4ee02bec999d9f922b108846d1cdb0316651
Adrian <adrian.cinca3@gmail.com>
2021-03-17 15:08:13 +0200

    Add log in layout

---
+++ app/src/main/java/com/example/wetherapp/LogInActivity.java
@@ -0,0 +1,34 @@
+package com.example.wetherapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class LogInActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in);
+//        TODO: Hyperlink to text
+//        TextView textView = findViewById(R.id.SingInTextView);
+//        String text = "I want THIS and THIS to be clickable";
+//        SpannableString ss = new SpannableString(text);
+//        ClickableSpan clickableSpan1 = new ClickableSpan() {
+//            @Override
+//            public void onClick(View widget) {
+//                Toast.makeText(LogInActivity.this, "One", Toast.LENGTH_SHORT).show();
+//            }
+//        };
+//
+//        ss.setSpan(clickableSpan1, 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//
+//        textView.setText(ss);
+//        textView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+}
--- app/src/main/java/com/example/wetherapp/MainActivity.java
+++ app/src/main/java/com/example/wetherapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.wetherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
