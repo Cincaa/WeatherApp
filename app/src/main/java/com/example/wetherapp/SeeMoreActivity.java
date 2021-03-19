2dda7332e2aa47b6da1535b218747addedaf43a0
Adrian <adrian.cinca3@gmail.com>
2021-03-19 13:57:50 +0200

    Add see more page

---
+++ app/src/main/java/com/example/wetherapp/SeeMoreActivity.java
@@ -0,0 +1,17 @@
+package com.example.wetherapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SeeMoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.see_more);
+
+    }
+}
