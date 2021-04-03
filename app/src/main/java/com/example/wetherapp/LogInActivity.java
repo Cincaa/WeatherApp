93f9754e4fdf7537abe3c9c4efaa4a4556d3d5ab
Adrian <adrian.cinca3@gmail.com>
2021-04-04 00:13:50 +0300

    Log in and register using Shared Preferences

--- app/src/main/java/com/example/wetherapp/LogInActivity.java
+++ app/src/main/java/com/example/wetherapp/LogInActivity.java
@@ -1,34 +1,63 @@
 package com.example.wetherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.method.LinkMovementMethod;
-import android.text.style.ClickableSpan;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
-public class LogInActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity{
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in);
-//        TODO: Hyperlink to text
-//        TextView textView = findViewById(R.id.SingInTextView);
-//        String text = "I want THIS and THIS to be clickable";
-//        SpannableString ss = new SpannableString(text);
-//        ClickableSpan clickableSpan1 = new ClickableSpan() {
-//            @Override
-//            public void onClick(View widget) {
-//                Toast.makeText(LogInActivity.this, "One", Toast.LENGTH_SHORT).show();
-//            }
-//        };
-//
-//        ss.setSpan(clickableSpan1, 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-//
-//        textView.setText(ss);
-//        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        final EditText etName = (EditText) findViewById(R.id.NameText);
+        final EditText etPassword = (EditText) findViewById(R.id.PasswordText);
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        Button btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = etName.getText().toString();
+                String password = etPassword.getText().toString();
+
+                SharedPreferences preferences = getSharedPreferences("MYPREFS", MODE_PRIVATE);
+
+                String userDetails = preferences.getString(user + password + "data", "Username or Passoword is Incorrect.");
+//                SharedPreferences.Editor editor = preferences.edit();
+//                editor.putString("display", userDetails);
+//                editor.commit();
+                if(!userDetails.contains("Username or Passoword is Incorrect.")) {
+                    Intent homePage = new Intent(LogInActivity.this, MainActivity.class);
+                    startActivity(homePage);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Username or Passoword is Incorrect.",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerScreen = new Intent(LogInActivity.this, RegisterActivity.class);
+                startActivity(registerScreen);
+            }
+        });
     }
+//    @Override
+//    public boolean onTouch(View v, MotionEvent event) {
+//        // check which textview it is and do what you need to do
+//        Intent registerScreen = new Intent(LogInActivity.this, RegisterActivity.class);
+//        startActivity(registerScreen);
+//        // return true if you don't want it handled by any other touch/click events after this
+//        return true;
+//    }
 }
